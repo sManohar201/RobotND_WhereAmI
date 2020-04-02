6a26a27689f39d6951110ebcda2bc2dfdde5c513
sam <spalania@mtu.edu>
2020-04-01 21:02:53 -0400

    Add image processing node

---
+++ src/ball_chaser/src/process_image.cpp
@@ -0,0 +1,16 @@
+#include "ros/ros.h"
+#include "ball_cahser/DriveToTarget.h"
+
+class ImageProcessor {
+    private:
+        
+    public:
+        ImageProcessor();
+        ~ImageProcessor() {};
+}
+
+int main(int argc, char ** argv) {
+    ros::init(argc, argv, "image_processing");
+
+    return 0;
+}
